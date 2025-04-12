87d1250297a1c984d3874fa5453fc0e5971da02c
Mithun Manohar <mithunmanohar79@gmail.com>
2025-04-12 07:58:53 +0000

    Initial setup of Dimensional Perspectives visualization

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import DimensionalPerspectives from './components/DimensionalPerspectives';
+
+function App() {
+  return (
+    <div className="App">
+      <DimensionalPerspectives />
+    </div>
+  );
+}
+
+export default App;
